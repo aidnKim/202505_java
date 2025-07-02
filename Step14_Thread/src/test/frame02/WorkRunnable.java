c3a3cd4e4269c9f865c385aab5dc9dfa8ac822ca
kimhyeonsoo <hyeonsookim2@gmail.com>
2025-07-02 15:13:02 +0900

    Step14_Thread 프로젝트 추가함

---
+++ Step14_Thread/src/test/frame02/WorkRunnable.java
@@ -0,0 +1,25 @@
+package test.frame02;
+/*
+ * 새로운 스레드 만드는 방법2
+ * 
+ * 1. Runnable 인터페이스를 구현한 클래스를 정의한다
+ * 2. run() 메소드를 강제 오버라이드한다
+ * 3. Thread 클래스로 객체를 생성하면서 해당 클래스로 만든 객체를 생성자의 인자로
+ *    전달한다
+ * 4. Thread 클래스로 만든 객체의 start() 메소드를 호출해서 스레드를 시작시킨다
+ */
+public class WorkRunnable implements Runnable{
+
+	//2.
+	@Override
+	public void run() {		
+		System.out.println("10 초 걸리는 작업을 시작합니다");
+		try {
+			//결과적으로 메인스레드는 10초간 sleep 상태로 된다(작업 중이라고 가정)
+			Thread.sleep(1000*10);
+		} catch (InterruptedException e1) {
+			e1.printStackTrace();
+		}
+		System.out.println("작업이 종료 되었습니다.");
+	}	
+}
